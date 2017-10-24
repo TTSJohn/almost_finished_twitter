8377ace824c05a768b02cf583d33ca350aa9fe4c
John Moranski <johnnymoranski21@gmail.com>
2017-10-24 10:52:20 -0500

    finished twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  serialize :following, Array 
   has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
